32936dd975318eede55006045b0af707f5ab403c
Kiril Vatev <vatev.1@gmail.com>
2026-01-11 12:39:51 -0500

    silent

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -7,38 +7,38 @@ export const HistoryEvent = {
 };
 
 window.history.back = (...args) => {
-  console.log('BACK', args);
+  // console.log('BACK', args);
 
   History.prototype.back.apply(history, args);
   window.dispatchEvent(new CustomEvent(HistoryEvent.back));
 };
 
 window.history.forward = (...args) => {
-  console.log('FORWARD', args);
+  // console.log('FORWARD', args);
 
   History.prototype.forward.apply(history, args);
   window.dispatchEvent(new CustomEvent(HistoryEvent.forward));
 };
 
 window.history.go = (...args) => {
-  console.log('GO', args);
+  // console.log('GO', args);
 
   History.prototype.go.apply(history, args);
   window.dispatchEvent(new CustomEvent(HistoryEvent.go));
 };
 
 window.history.pushState = (...args) => {
-  console.log('PUSHSTATE', args);
+  // console.log('PUSHSTATE', args);
 
   History.prototype.pushState.apply(history, args);
   window.dispatchEvent(new CustomEvent(HistoryEvent.pushState));
 };
 
 window.history.replaceState = (...args) => {
-  console.log('REPLACESTATE', args);
+  // console.log('REPLACESTATE', args);
 
   History.prototype.replaceState.apply(history, args);
   window.dispatchEvent(new CustomEvent(HistoryEvent.replaceState));
 };
 
-console.log('PATCHED HISTORY');
+// console.log('PATCHED HISTORY');
