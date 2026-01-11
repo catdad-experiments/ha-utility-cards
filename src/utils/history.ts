a83f7f7590b9f9d765675b8053ca18eae05e28aa
Kiril Vatev <vatev.1@gmail.com>
2026-01-11 12:33:03 -0500

    events should fire after history is applied

--- src/utils/history.ts
+++ src/utils/history.ts
@@ -9,36 +9,36 @@ export const HistoryEvent = {
 window.history.back = (...args) => {
   console.log('BACK', args);
 
-  window.dispatchEvent(new CustomEvent(HistoryEvent.back));
   History.prototype.back.apply(history, args);
+  window.dispatchEvent(new CustomEvent(HistoryEvent.back));
 };
 
 window.history.forward = (...args) => {
   console.log('FORWARD', args);
 
-  window.dispatchEvent(new CustomEvent(HistoryEvent.forward));
   History.prototype.forward.apply(history, args);
+  window.dispatchEvent(new CustomEvent(HistoryEvent.forward));
 };
 
 window.history.go = (...args) => {
   console.log('GO', args);
 
-  window.dispatchEvent(new CustomEvent(HistoryEvent.go));
   History.prototype.go.apply(history, args);
+  window.dispatchEvent(new CustomEvent(HistoryEvent.go));
 };
 
 window.history.pushState = (...args) => {
   console.log('PUSHSTATE', args);
 
-  window.dispatchEvent(new CustomEvent(HistoryEvent.pushState));
   History.prototype.pushState.apply(history, args);
+  window.dispatchEvent(new CustomEvent(HistoryEvent.pushState));
 };
 
 window.history.replaceState = (...args) => {
   console.log('REPLACESTATE', args);
 
-  window.dispatchEvent(new CustomEvent(HistoryEvent.replaceState));
   History.prototype.replaceState.apply(history, args);
+  window.dispatchEvent(new CustomEvent(HistoryEvent.replaceState));
 };
 
 console.log('PATCHED HISTORY');
